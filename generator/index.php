ef5db0e2c6fb6155cca82ca1c04f66f4e366388c
James Padolsey <github@padolsey.net>
2013-07-30 19:51:35 +0100

    correct git ribbon img

--- generator/index.php
+++ generator/index.php
@@ -102,7 +102,7 @@ if ($homepage && $homepage_url !== '/') {
 </head>
 <body>
 
-	<a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+	<a href="https://github.com/yip/concept/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork Yip/Concept on GitHub"></a>
 
 	<nav>
 		<a class="brand" href="<?php echo $homepage_url;?>">
